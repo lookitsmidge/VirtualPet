6a4952b3d0f8fdeca961a875df3afcbad9a0aa7f
James Martland <lookitsmidge@gmail.com, 24233781@edgehill.ac.uk>
2020-03-09 10:43:16 +0000

    Update Animal.java
    Added abstract methods

--- VirtualPet/src/animals/Animal.java
+++ VirtualPet/src/animals/Animal.java
@@ -4,8 +4,8 @@ public abstract class Animal extends ProcessorTemplate {
 	//these are the universal attributes of all of the animals that are going to be created
 	private String name;
 	private int age;
-	protected int happiness = 10; // out of 20
-	protected int fitness = 5; // out of 10
+//	protected int happiness = 10; // out of 20
+//	protected int fitness = 5; // out of 10
 	
 	/**
 	 * This is the constructor for the class
@@ -16,6 +16,18 @@ public abstract class Animal extends ProcessorTemplate {
 		
 		//Run a Thread Test? to decrease Happiness
 	}
+	
+	public abstract void speak();
+
+	public abstract void eat();
+
+	public abstract void showLove();
+
+	public abstract void sleep();
+
+	public abstract void wake();
+
+	public abstract void play();
 	
 	/**
 	 * This is to set the value of the class variable age
